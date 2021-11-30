40b4c3ccb169ff95808ab2cbaa38dcc5ea023567
Idemudia Anita Eghonghon <Ieghonghon@gmail.com>
2021-11-30 10:51:15 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,9 +12,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
- getAlbum(id: number): Observable<Album[]> {
+ getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response =>
-      <Album[]>response.json()
+      <Album>response.json()
     );
   }
 
